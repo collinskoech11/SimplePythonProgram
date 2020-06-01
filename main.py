80d761d1e8a9200e81bacdd2b3cdda93c5aadfa8
collinskoech11 <collinskoechck34@gmail.com>
2020-06-01 19:43:42 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+project title- main.py
+task- creating variables and printing them(also trying to use comments)
+description- variables are descriptive of a programmers life)
+"""
+
+PName = "Collins"
+title = "programmer"
+yearsOfExperience = 3
+favouriteLanguage = "Python"
+OperatingSystem = "Linux"
+Distro = "ubuntu"
+
+print(PName)
+print(title)
+print(yearsOfExperience)
+print(favouriteLanguage)
+print(OperatingSystem)
+print(Distro)
